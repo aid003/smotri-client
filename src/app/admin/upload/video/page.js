05df6231bac3153ebc28d6f798fa4f94af53cca8
aid003 <teremochek859@gmail.com>
2023-07-20 12:07:17 +0300

    dsds

--- src/app/admin/upload/video/page.js
+++ src/app/admin/upload/video/page.js
@@ -2,35 +2,147 @@
 
 import { useState } from "react";
 
+const initialValues = {
+  title: "",
+  ratingFilm: 0,
+  postersUrl: "psYrl",
+  yearCreate: 2022,
+  countries: "",
+  gendre: "",
+  content: "",
+  ageRestriction: 16,
+  description: "",
+  actors: "",
+  url: "url",
+  TitleSeo: "",
+  DescriptionSeo: "",
+};
+
+const initialState = { values: initialValues };
+
 const Page = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [state, setState] = useState(initialState);
 
   const handleChange = (e) => {
-    console.log(e.target.value);
-    setSelectedFile(e.target.value);
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setSelectedFile(e.target.files[0]);
   };
 
-  const handleUpload = async () => {
-    console.log(selectedFile);
+  const { values } = state;
+
+  const updateVideoInfoHandler = ({ target }) => {
+    setState((prev) => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [target.name]: target.value,
+      },
+    }));
+
+    console.log(values);
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    setState((prev) => ({ ...prev }));
     if (!selectedFile) {
       alert("Файл не выбран, выберите файл!");
       return;
     }
-
     const formData = new FormData();
     formData.append("file", selectedFile);
+    formData.append("information", JSON.stringify(values));
 
-    const res = await fetch(process.env.UPLOAD_PATH, {
+    console.log(formData);
+
+    // process.env.UPLOAD_PATH
+
+    const res = await fetch("http://localhost:5005/api/upload-video-file", {
       method: "POST",
       body: formData,
     });
+    const data = await res.json();
+    console.log(data);
   };
 
   return (
-    <div>
-      <input type="file" accept=".mp4" onChange={handleChange}></input>
+    <form encType="multipart/form-data">
+      <input
+        type="file"
+        accept=".mp4"
+        name="file"
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        name="title"
+        placeholder="title"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="number"
+        name="ratingFilm"
+        placeholder="ratingFilm"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="number"
+        name="yearCreate"
+        placeholder="yearCreate"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="countries"
+        placeholder="countries"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="gendre"
+        placeholder="gendre"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="content"
+        placeholder="content"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="number"
+        name="ageRestriction"
+        placeholder="ageRestriction"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="description"
+        placeholder="description"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="actors"
+        placeholder="actors"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="TitleSeo"
+        placeholder="TitleSeo"
+        onChange={updateVideoInfoHandler}
+      ></input>
+      <input
+        type="text"
+        name="DescriptionSeo"
+        placeholder="DescriptionSeo"
+        onChange={updateVideoInfoHandler}
+      ></input>
       <input type="submit" onClick={handleUpload}></input>
-    </div>
+    </form>
   );
 };
 
