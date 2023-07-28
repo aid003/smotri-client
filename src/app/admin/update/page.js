3329ceade33af9ea8403c78a6a5b05de90756043
aid003 <teremochek859@gmail.com>
2023-07-28 03:14:42 +0300

    v0.1.0

--- src/app/admin/update/page.js
+++ src/app/admin/update/page.js
@@ -1,14 +1,46 @@
 "use client";
 import { customFetch } from "@/middleware/customFetch";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Page = () => {
   const [titles, setTitles] = useState([]);
   const [fields, setFields] = useState([]);
   const [pickedTitle, setPickedTitle] = useState();
-    const [pickedField, setPickedField] = useState();
-    
-    const [value, setValue] = useState('')
+  const [pickedField, setPickedField] = useState();
+
+  const [currentData, setCurrentData] = useState();
+
+  const [value, setValue] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    async function validator() {
+      const token = localStorage.getItem("colorTheme");
+      if (!token) {
+        router.push("/admin/login");
+      }
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_PATH}login-user-with-token/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { isValidToken, role } = await response.json();
+
+      if (!isValidToken) {
+        router.push("/admin/login");
+      }
+
+      if (role === "User") {
+        router.push("/admin/login");
+      }
+    }
+    validator();
+  }, [router]);
 
   useEffect(() => {
     async function getTitles() {
@@ -27,43 +59,58 @@ const Page = () => {
     getFields();
   }, []);
 
-  const filmTitlePicker = (title) => {
-    setPickedTitle(title);
+  const filmTitlePicker = ({ target }) => {
+    setPickedTitle(target.value);
   };
-    
-    const updateValueHandler = () => {}
+
+  const filmFieldPicker = ({ target }) => {
+    setPickedField(target.value);
+  };
+
+  const updateValueHandler = ({ target }) => {
+    setValue(target.value);
+  };
+
+  const uploadHandler = async (e) => {
+    if (!pickedField || !pickedTitle || !value) {
+      alert(`Заполните все поля: ${pickedField} ${pickedTitle} ${value}`);
+    }
+    const entry = { title: pickedTitle, field: pickedField, value: value };
+    const sendUpdate = await customFetch(
+      `${process.env.NEXT_PUBLIC_SERVER_PATH}change-film-fields`,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(entry),
+      }
+    );
+    setCurrentData(JSON.stringify(sendUpdate.data));
+  };
 
   return (
     <div>
-      <select>
+      <select onChange={filmTitlePicker}>
+        <option value={false}>Выберите фильм</option>
         {titles.map((el) => (
-          <option
-            value={el.title}
-            key={el.title}
-            onClick={(e) => {
-              e.preventDefault();
-              filmTitlePicker(el.title);
-            }}
-          >
+          <option value={el.title} key={el.title}>
             {el.title}
           </option>
         ))}
       </select>
-      <select>
+      <select onChange={filmFieldPicker}>
+        <option value={false}>Выберите поле</option>
         {fields.map((el) => (
-          <option
-            value={el}
-            key={el}
-            onClick={(e) => {
-                e.preventDefault();
-                setPickedField(el)
-            }}
-          >
+          <option value={el} key={el}>
             {el}
           </option>
         ))}
-          </select>
-          <input type="text" onChange={updateValueHandler}></input>
+      </select>
+      <input type="text" onChange={updateValueHandler}></input>
+      <button onClick={uploadHandler}>ok</button>
+      <div>{currentData}</div>
     </div>
   );
 };
